fc889e7d28d5f480715a23fdd0acf6e628738723
Sheep Sun <sunxiaoyang2003@gmail.com>
2020-03-15 10:48:29 +0800

    Revert "Add "fastboot oem dump-regulators" to dump current regulator state"
    This reverts commit 1fdc25c45483a63b4313d4dd32a4bec0932992e4.

--- dev/pmic/pm8x41/rules.mk
+++ dev/pmic/pm8x41/rules.mk
@@ -5,8 +5,7 @@ INCLUDES += -I$(LOCAL_DIR)/include
 OBJS += \
 	$(LOCAL_DIR)/pm8x41.o \
 	$(LOCAL_DIR)/pm8x41_adc.o \
-	$(LOCAL_DIR)/pm8x41_wled.o \
-	$(LOCAL_DIR)/pm8x41_regulator.o
+	$(LOCAL_DIR)/pm8x41_wled.o
 
 ifeq ($(ENABLE_PON_VIB_SUPPORT),true)
 OBJS += \
